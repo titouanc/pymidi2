2b1857e295022c804a7a700453a41956c21fc93a
Titouan Christophe <moiandme@gmail.com>
2025-02-15 15:24:16 +0100

    Initial commit
    Signed-off-by: Titouan Christophe <moiandme@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# netmidi2
+
+A pure Python implementation of MIDI2 over UDP, based on
+
+[**User Datagram Protocol for Universal MIDI Packets** _Network MIDI 2.0 (UDP) Transport Specification_](https://drive.google.com/file/d/1dtsOgMLbtif9Fp-OaZhwnRs9an4dn3uv/edit)
+
+This implementation is notably incomplete, and serves as a workbench for other developments
---
+++ tests/test_netmidi.py
@@ -0,0 +1,34 @@
+import pytest
+
+from netmidi2.core import CommandCode, CommandPacket, MIDIUDPPacket
+
+def test_decode_valid_packet():
+    pkt, rest = CommandPacket.parse(b"\xff\x01\x12\x34****")
+    assert rest == b""
+    assert pkt.command == CommandCode.UMP_DATA
+    assert pkt.specific_data == 0x1234
+    assert pkt.payload == b"****"
+
+
+def test_decode_with_rest():
+    pkt_bytes = b"\xff\x01\x12\x34****"
+    pkt, rest = CommandPacket.parse(2 * pkt_bytes)
+    assert rest == pkt_bytes
+
+
+def test_encode_valid_packet():
+    pkt = CommandPacket(command=CommandCode.UMP_DATA, specific_data=2047, payload=b"Coucou  ")
+    assert bytes(pkt) == b"\xff\x02\x07\xffCoucou  "
+
+
+def test_encode_unaligned_payload():
+    with pytest.raises(ValueError):
+        CommandPacket(command=CommandCode.UMP_DATA, payload="unaligned")
+
+
+def test_decode_udp_packet():
+    udp_pkt = MIDIUDPPacket.parse(b"MIDI\xff\x01\x12\x34****")
+    [pkt] = udp_pkt.commands
+    assert pkt.command == CommandCode.UMP_DATA
+    assert pkt.specific_data == 0x1234
+    assert pkt.payload == b"****"
