a87b77bc1f188cafa17b7d64289ffc63758e86e2
Titouan Christophe <titouan.christophe@mind.be>
2025-06-19 15:35:45 +0200

    Add command send1
    Signed-off-by: Titouan Christophe <titouan.christophe@mind.be>

--- pymidi2/__main__.py
+++ pymidi2/__main__.py
@@ -1,8 +1,9 @@
 import argparse
 import logging
 import time
-from binascii import hexlify
+from binascii import hexlify, unhexlify
 from itertools import chain
+from urllib.parse import urlparse
 
 from pymidi2 import smf, ump
 from pymidi2.endpoint import UMPEndpoint
@@ -13,6 +14,16 @@ from pymidi2.transport import (
     UDPTransport,
     UserPasswordRequiredError,
 )
+
+
+def get_ump_group(endpoint_url: str, default: int = 0) -> int:
+    ump_group = default
+    url = urlparse(endpoint_url)
+    if url.fragment:
+        ump_group = int(url.fragment)
+    if not (0 <= ump_group < 16):
+        raise ValueError(f"Invalid UMP group number {ump_group}")
+    return ump_group
 
 
 def print_ep_topology(endpoint_url: str) -> None:
@@ -55,8 +66,12 @@ def topo_endpoint(args) -> None:
 
 
 def play_file(args) -> None:
-    endpoint = UMPEndpoint.open(args.endpoint_url) if args.endpoint_url else None
+    endpoint = None
     ump_group = 0
+
+    if args.endpoint_url:
+        ump_group = get_ump_group(args.endpoint_url, ump_group)
+        endpoint = UMPEndpoint.open(args.endpoint_url)
 
     bpm = 120.0
     start = time.monotonic()
@@ -79,13 +94,28 @@ def play_file(args) -> None:
                 prefix = bytes([0x20 + ump_group])
                 event_bytes = (prefix + ev.data).ljust(4, b"\x00")
                 pkts.append(ump.UMP.parse([int.from_bytes(event_bytes)]))
-            print(hexlify(ev.data).decode().upper())
+
+            if not args.quiet:
+                print(hexlify(ev.data).decode().upper())
 
         while (time.monotonic() - start) / 60 < beat / bpm:
             pass
 
         if endpoint is not None:
             endpoint.sendmany(pkts)
+
+
+def send_midi1(args):
+    ump_group = get_ump_group(args.endpoint_url)
+
+    pkts = []
+    for ev in args.event:
+        prefix = bytes([0x20 + ump_group])
+        event_bytes = (prefix + unhexlify(ev)).ljust(4, b"\x00")
+        pkts.append(ump.UMP.parse([int.from_bytes(event_bytes)]))
+
+    endpoint = UMPEndpoint.open(args.endpoint_url)
+    endpoint.sendmany(pkts)
 
 
 def main():
@@ -146,7 +176,17 @@ parser_play.add_argument(
     "--endpoint_url",
     help="URL of the UMP endpoint to play the file to",
 )
+parser_play.add_argument(
+    "-q",
+    "--quiet",
+    action="store_true",
+    help="Do not print MIDI events to stdout as they are played",
+)
 
+parser_send1 = subparsers.add_parser("send1", help="Send MIDI1 events")
+parser_send1.set_defaults(func=send_midi1)
+parser_send1.add_argument("endpoint_url", help="URL of the UMP endpoint to connect to")
+parser_send1.add_argument("event", nargs="+")
 
 if __name__ == "__main__":
     main()
